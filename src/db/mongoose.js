a283d0b6ead7ecd4e2d22bcd14740fbc35da762f
Luis Villarreal <ldvo9606@gmail.com>
2019-05-06 16:56:32 -0500

    Updated env variable name

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const config = require('../config')
 
-var connectionURL = process.env.CONNECTION_URL || config.connectionURL
+var connectionURL = process.env.DB_CONNECTION_STRING || config.connectionURL
 
 mongoose.connect( connectionURL, {
   useNewUrlParser: true,
